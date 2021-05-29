ae2775812d9edab2a0e935458ddb2ada299b11d4
shuri-kimura <shuri-kimura@users.noreply.github.com>
2021-05-29 06:27:35 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -37,6 +37,47 @@ bool check_dup(string text, int size){
     return true;
 }
 
+void selectionSort(string data, int left, int right) {
+    int start;
+    int i;
+    char min;
+    int i_min;
+    char tmp;
+
+    /* データ数が１の場合はソート済みなのでソート終了 */
+    if (left == right) {
+        return;
+    }
+
+    /* ソート範囲（開始点）の初期化 */
+    start = left;
+
+    /* ソート範囲を狭めながらソート処理 */
+    for (start = left; start < right; start++) {
+
+        /* ひとまずソート範囲の先頭を最小値とみなす */
+        i_min = start;
+        min = data[i_min];
+
+        /* ソート範囲の中から最小値を探索 */
+        for (i = start; i <= right; i++) {
+            if (min > data[i]) {
+                /* 最小値とその値を持つインデックスを更新 */
+                min = data[i];
+                i_min = i;
+            }
+        }
+
+        if (start != i_min) {
+
+            /* ソート範囲の先頭と最小値を交換 */
+            tmp = data[start];
+            data[start] = data[i_min];
+            data[i_min] = tmp;
+        }
+    }
+}
+
 
 int main(int argc, string argv[])
 {
@@ -45,11 +86,13 @@ int main(int argc, string argv[])
         int counter = 0;
         bool flag;
         int  n = strlen(argv[1]);
+        char check_key[n];
         for (int i = 0; i < n; i++) //char by char from key
         {
             if ((argv[1][i] >= 'a' && argv[1][i] <= 'z') || (argv[1][i] >= 'A' && argv[1][i] <= 'Z')) //check and transform char
             {
                 argv[1][i] = toupper(argv[1][i]);
+                check_key[i] = argv[1][i];
                 counter += 1;
             }
             else
@@ -63,7 +106,9 @@ int main(int argc, string argv[])
             flag = true;
         }
 
-        //flag = flag && check_dup(argv[1], n);
+
+        selectionSort(check_key, 0, n-1);//check_key sort
+        flag = flag && check_dup(check_key, n);
 
         if (flag == true)
         {
