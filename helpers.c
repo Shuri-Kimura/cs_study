1eb3d1ec6f7aea14d203a9274d866483c7c9fc75
shuri-kimura <shuri-kimura@users.noreply.github.com>
2021-06-12 09:54:37 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -163,7 +163,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 image_[i][j].rgbtGreen = ((image[i][j].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j - 1].rgbtGreen + image[i][j - 1].rgbtGreen + image[i + 1][j].rgbtGreen + image[i + 1][j - 1].rgbtGreen) / 6.0) + 0.5;
                 image_[i][j].rgbtBlue = ((image[i][j].rgbtBlue + image[i - 1][j].rgbtBlue + image[i - 1][j - 1].rgbtBlue + image[i][j - 1].rgbtBlue + image[i + 1][j].rgbtBlue + image[i + 1][j - 1].rgbtBlue) / 6.0) + 0.5;
             }
-            else if (j == height - 1)
+            else if (i == height - 1)
             {
                 image_[i][j].rgbtRed = ((image[i][j].rgbtRed + image[i][j - 1].rgbtRed + image[i - 1][j - 1].rgbtRed + image[i - 1][j].rgbtRed + image[i - 1][j + 1].rgbtRed + image[i][j + 1].rgbtRed) / 6.0) + 0.5;
                 image_[i][j].rgbtGreen = ((image[i][j].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j - 1].rgbtGreen + image[i - 1][j].rgbtGreen + image[i - 1][j + 1].rgbtGreen + image[i][j + 1].rgbtGreen) / 6.0) + 0.5;
