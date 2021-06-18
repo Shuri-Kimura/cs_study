3119df93f34a49bf7e91b9f1861f0730ac5c5ab7
shuri-kimura <shuri-kimura@users.noreply.github.com>
2021-06-18 13:36:01 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -40,7 +40,7 @@ bool check(const char *word)
     int hashval = hash(s_word);
     ptr = malloc(sizeof(struct node));
     ptr = table[hashval];
-    //printf("searchword: %s,hashval: %d\n",s_word,hashval);
+    printf("searchword: %s,hashval: %d\n",s_word,hashval);
     while(ptr != NULL)
     {
         if (strcmp(ptr->word,s_word) == 0)
