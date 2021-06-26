da289e1cd6b370ad380fe620c6863cb9c81b194c
shuri-kimura <shuri-kimura@users.noreply.github.com>
2021-06-26 06:23:56 +0000

    automated commit by check50 [check50=True]

---
+++ mario.py
@@ -0,0 +1,36 @@
+
+def main():
+    height = -1
+    # input number
+    while (True):
+        height = int(input("Height: "))
+        if (height <= 8 or height >= 1):
+            break;
+
+    # output block number:8
+    if (height == 8):
+        print("       #  #\n      ##  ##\n     ###  ###\n    ####  ####\n   #####  #####\n  ######  ######\n #######  #######\n########  ########\n")
+    # output block number:7
+    elif (height == 7):
+        print("      #  #\n     ##  ##\n    ###  ###\n   ####  ####\n  #####  #####\n ######  ######\n#######  #######\n")
+    # output block number:6
+    elif (height == 6):
+        print("     #  #\n    ##  ##\n   ###  ###\n  ####  ####\n #####  #####\n######  ######\n")
+    # output block number:5
+    elif (height == 5):
+        print("    #  #\n   ##  ##\n  ###  ###\n ####  ####\n#####  #####\n")
+    # output block number:4
+    elif (height == 4):
+        print("   #  #\n  ##  ##\n ###  ###\n####  ####\n")
+    # output block number:3
+    elif (height == 3):
+        print("  #  #\n ##  ##\n###  ###\n")
+    # output block number:2
+    elif (height == 2):
+        print(" #  #\n##  ##\n")
+    # output block number:1
+    else:
+        print("#  #\n")
+
+if __name__ == "__main__":
+    main()
