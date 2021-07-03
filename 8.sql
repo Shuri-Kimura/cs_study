cd190d62d43bb3491bff9ea4ba11062a79d9e3ee
shuri-kimura <shuri-kimura@users.noreply.github.com>
2021-07-03 12:10:48 +0000

    automated commit by check50 [check50=True]

--- 8.sql
+++ 8.sql
@@ -1,1 +1,1 @@
-SELECT name FROM peple JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE title = "Toy Story";
+SELECT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE title = "Toy Story";
